338fb1185b476c407035c9d6ff82903b390abc2d
Xiaolin Zhang <leoncamel@gmail.com>
2024-10-11 04:52:29 +0800

    Init commit from AstroVim's template

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
+  { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
+  { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  -- import/override with your plugins folder
+}
