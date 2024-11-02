0150dd14a4fb22c286f57dc3ecb6cb04e7ed62f9
Xiaolin Zhang <leoncamel@gmail.com>
2024-11-02 14:09:21 +0800

    Disable preview.nvim

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -1,27 +1,29 @@
----@type LazySpec
-return {
-  "https://gitlab.com/itaranto/preview.nvim",
-  version = "*",
-  opts = {
-    previewers_by_ft = {
-      markdown = {
-        name = "pandoc_wkhtmltopdf",
-        renderer = { type = "command", opts = { cmd = { "zathura" } } },
-      },
-      plantuml = {
-        name = "plantuml_text",
-        renderer = { type = "buffer" },
-      },
-      groff = {
-        name = "groff_ms_pdf",
-        renderer = { type = "command", opts = { cmd = { "zathura" } } },
-      },
-    },
-    previewers = {
-      plantuml_svg = {
-        args = { "-pipe", "-tpng" },
-      },
-    },
-    render_on_write = true,
-  },
-}
+return {}
+
+-- ---@type LazySpec
+-- return {
+--   "https://gitlab.com/itaranto/preview.nvim",
+--   version = "*",
+--   opts = {
+--     previewers_by_ft = {
+--       markdown = {
+--         name = "pandoc_wkhtmltopdf",
+--         renderer = { type = "command", opts = { cmd = { "zathura" } } },
+--       },
+--       plantuml = {
+--         name = "plantuml_text",
+--         renderer = { type = "buffer" },
+--       },
+--       groff = {
+--         name = "groff_ms_pdf",
+--         renderer = { type = "command", opts = { cmd = { "zathura" } } },
+--       },
+--     },
+--     previewers = {
+--       plantuml_svg = {
+--         args = { "-pipe", "-tpng" },
+--       },
+--     },
+--     render_on_write = true,
+--   },
+-- }
